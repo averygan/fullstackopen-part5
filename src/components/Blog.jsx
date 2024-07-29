877cc0040d65acbed22fc46d0a65dd9e482fbbcd
averygan <averygan19@gmail.com>
2024-07-29 15:08:26 +0800

    completed ex 5.7 - added view/hide toggling for individual blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+  }
+  
+  return (
+    <div style={blogStyle}>
+      {/* when not visible -> show title and view button */}
+      <div style={hideWhenVisible}>
+        {blog.title} <button onClick={toggleVisibility}>view</button>
+      </div>
+
+      {/* when visible -> show details and hide button */}
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button>
+        <div>URL: {blog.url}</div>
+        <div>likes: {blog.likes}</div>
+        <div>User: {blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
