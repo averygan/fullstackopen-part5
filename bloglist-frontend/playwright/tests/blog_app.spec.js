6a57abbd1f5e33b81b840966f509b08db4a4cf24
averygan <averygan19@gmail.com>
2024-08-05 00:54:15 +0800

    completed ex 5.23 - added function to retrieve token and create blogs with likes in test; checks that bloglist is sorted

--- bloglist-frontend/playwright/tests/blog_app.spec.js
+++ bloglist-frontend/playwright/tests/blog_app.spec.js
@@ -1,12 +1,12 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
-const { loginWith, createBlog, loginDummy } = require('./helper')
+const { loginWith, createBlog, loginDummy, loginToken, createBlogWithToken } = require('./helper')
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
     // empty db
-    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('/api/testing/reset')
     // create a user
-    await request.post('http://localhost:3003/api/users', {
+    await request.post('/api/users', {
       data: {
         name: 'Jane Doe',
         username: 'janed',
@@ -71,6 +71,38 @@ describe('Blog app', () => {
         await page.getByRole('button', { name: 'view' }).click()
         await expect(page.getByRole('button', { name: 'delete' })).toHaveCount(0)
       })
+    })
+  })
+
+  describe('3 blogs with varying likes', () => {
+    let token
+
+    beforeEach(async ({ page, request }) => {
+      token = await loginToken(page, 'janed', 'test123')
+
+      await createBlogWithToken(request, 'highest blog', 'top author', 'www.test.com', 123, token)
+      await createBlogWithToken(request, 'lowest blog', 'bot author', 'www.test.com', 2, token)
+      await createBlogWithToken(request, 'mid blog', 'mid author', 'www.test.com', 20, token)
+
+      await page.goto('http://localhost:5173')
+    })
+
+    test('check that three blogs are created', async ({ page }) => {
+      // check that all 3 blogs are present
+      await expect(page.locator('.bloglist >> text="highest blog"')).toContainText('highest blog')
+      await expect(page.locator('.bloglist >> text="lowest blog"')).toContainText('lowest blog')
+      await expect(page.locator('.bloglist >> text="mid blog"')).toContainText('mid blog')
+    })
+
+    test('check that blogs are sorted', async ({ page }) => {
+      const blogEntries = page.locator('.bloglist')
+      const firstBlog = blogEntries.nth(0)
+      const secondBlog = blogEntries.nth(1)
+      const thirdBlog = blogEntries.nth(2)
+
+      await expect(firstBlog).toContainText('highest blog')
+      await expect(secondBlog).toContainText('mid blog')
+      await expect(thirdBlog).toContainText('lowest blog')
     })
   })
 })
